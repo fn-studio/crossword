8b0bb9b011964cc64cec3ca91e00ccd944444c0c
Nathan Shelly <nsnshelly@gmail.com>
2021-03-07 22:55:23 -0800

    tweaks

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,47 @@
 /** @jsx jsx */
 import { jsx } from '@emotion/react';
-import React from 'react';
-
 import { Trie } from 'prefix-trie-ts';
+import { useState } from 'react';
+// import {useState} from '@
 
 import { words } from './wordlist';
 
 const trie = new Trie(words);
 console.log(trie.getPrefix('bat'));
 
+const items = [...Array(25)];
+const initialInputs = items.reduce((acc, key) => ({ ...acc, [key]: '' }));
 const App: React.FC = () => {
+  const [inputs, setInputs] = useState(initialInputs);
+
   return (
     <div
       css={{
         display: 'grid',
         gridTemplateColumns: 'repeat(5, 1fr)',
         backgroundColor: 'lightcoral',
-        margin: '0 auto',
-        width: '640px',
+        // margin: '0 auto',
+        width: '100vw',
+        // width: '640px',
         height: '640px',
       }}
     >
-      {[...Array(25)].map((_, i) => (
-        <div
+      {items.map((_, i) => (
+        <input
+          type="text"
           key={i}
           css={{
             display: 'flex',
             alignItems: 'center',
             justifyContent: 'center',
             backgroundColor: 'cyan',
+            fontSize: '30px',
           }}
-        >
-          hi
-        </div>
+          onChange={(event) => {
+            setInputs({ ...inputs, [i]: event.target.value.slice(-1) });
+          }}
+          value={inputs[i]}
+        />
       ))}
     </div>
   );
