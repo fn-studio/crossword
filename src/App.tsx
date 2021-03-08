e61e659d76270622c958278667a6782187a52f5c
Nathan Shelly <nsnshelly@gmail.com>
2021-03-07 22:25:43 -0800

    make a simple grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,39 @@
 /** @jsx jsx */
 import { jsx } from '@emotion/react';
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 
 import { Trie } from 'prefix-trie-ts';
 
 import { words } from './wordlist';
 
 const trie = new Trie(words);
+console.log(trie.getPrefix('bat'));
 
 const App: React.FC = () => {
-  // Create the count state.
-  const [count, setCount] = useState(0);
-  console.log(trie.getPrefix('bat'));
-
-  // Create the counter (+1 every second).
-  useEffect(() => {
-    const timer = setTimeout(() => setCount(count + 1), 1000);
-    return () => clearTimeout(timer);
-  }, [count, setCount]);
-  // Return the App component.
   return (
-    <div css={{ backgroundColor: 'darkorchid' }}>
-      <header>
-        <img />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <p>
-          Page has been open for <code>{count}</code> seconds.
-        </p>
-        <p>
-          <a
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          ></a>
-        </p>
-      </header>
+    <div
+      css={{
+        display: 'grid',
+        gridTemplateColumns: 'repeat(5, 1fr)',
+        backgroundColor: 'lightcoral',
+        margin: '0 auto',
+        width: '640px',
+        height: '640px',
+      }}
+    >
+      {[...Array(25)].map((_, i) => (
+        <div
+          key={i}
+          css={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            backgroundColor: 'cyan',
+          }}
+        >
+          hi
+        </div>
+      ))}
     </div>
   );
 };
